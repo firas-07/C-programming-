f4c2956d278ff574543c9333ea26a9b14412ded6
firas-07 <112752858+firas-07@users.noreply.github.com>
2022-10-06 20:36:32 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("\nEnter two numbers to add");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\nSum of entered numbers= %d",c);
+}
+
---
+++ main (18).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a;
+    printf("Enter a integer:");
+    scanf("%d",&a);
+    printf("\nThe integer you have entered is %d", a);
+}
